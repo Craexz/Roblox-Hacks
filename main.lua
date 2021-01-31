adf1e07ffee6f1b90637331844726ac4b8dfe409
Craexz <benhershey08@gmail.com>
2021-01-31 03:21:41 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,32 @@
+local gameid = game.PlaceId
+
+-- Project Lazarus
+if gameid == 443406476 then
+	loadstring(game:HttpGet("https://pastebin.com/raw/ty1aYq0u"))();
+end
+
+-- Big Paintball
+if gameid == 3527629287 then
+	loadstring(game:HttpGet("https://pastebin.com/raw/GtudyLtE"))();
+end
+
+-- My Restaraunt
+if gameid == 4490140733 then
+	loadstring(game:HttpGet("https://pastebin.com/raw/Csr2LzA3"))();
+end
+
+-- Prison Life
+if gameid == 155615604 then
+ 	loadstring(game:HttpGet("https://pastebin.com/raw/PR79FuHb"))();
+end
+
+-- Arsenal
+if gameid == 286090429 then
+	 loadstring(game:HttpGet(("https://pastebin.com/raw/yCrBkPaY"), true))();
+end
+
+-- Weight Lifting Simulator II
+if gameid == 985510407 then
+	 loadstring(game:HttpGet("https://pastebin.com/raw/r4pLKeK8"))()
+end
+
