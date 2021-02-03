19baf2fc3e00f52dfafe2e301ee5a78e6c7dbe90
Craexz <benhershey08@gmail.com>
2021-02-03 00:29:09 +0000

    added game detection

--- main.lua
+++ main.lua
@@ -2,57 +2,68 @@ local gameid = game.PlaceId
 
 -- Project Lazarus
 if gameid == 443406476 then
+  local detectedgame = 'Project Lazarus'
 	loadstring(game:HttpGet("https://pastebin.com/raw/H64HYzH0"))();
 end
 
 -- Big Paintball
 if gameid == 3527629287 then
+local detectedgame = 'Big Paintball'
 	loadstring(game:HttpGet("https://pastebin.com/raw/GtudyLtE"))();
 end
 
 -- My Restaraunt
 if gameid == 4490140733 then
+  local detectedgame = 'My Restaraunt'
 	loadstring(game:HttpGet("https://pastebin.com/raw/Csr2LzA3"))();
 end
 
 -- Prison Life
 if gameid == 155615604 then
+local detectedgame = 'Prison Life'
  	loadstring(game:HttpGet("https://pastebin.com/raw/PR79FuHb"))();
 end
 
 -- Arsenal
 if gameid == 286090429 then
+   local detectedgame = 'Arsenal'  
 	 loadstring(game:HttpGet(("https://pastebin.com/raw/yCrBkPaY"), true))();
 end
 
 -- Weight Lifting Simulator II
 if gameid == 985510407 then
+   local detectedgame = 'Weight Lifting Simulator II'
 	 loadstring(game:HttpGet("https://pastebin.com/raw/r4pLKeK8"))()
 end
 
 -- Sharkbite
 if gameid == 734159876 then
+   local detectedgame = 'Sharkbite'
    loadstring(game:GetObjects("rbxassetid://3623753581")[1].Source)()
 end
 
 -- Jailbreak
 if gameid == 606849621 then
+  local detectedgame = 'Jailbreak'
   loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Scripts/master/Vynixius%20Jailbreak", true))()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/HazeWasTaken/JailedHax/master/PayPal"))()
 end
 
 -- Tower Of Hell
 if gameid == 1962086868 then
+  local detectedgame = 'Tower Of Hell'
   loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
 end
 
 -- Phantom Forces
 if gameid == 292439477 then
+  local detectedgame = 'Phantom Forces'
   loadstring(game:HttpGet("https://raw.githubusercontent.com/ZinityDrops/OwlHubLink/master/OwlHubBack.lua"))();
 end
 
 -- Adopt Me
 if gameid == 920587237 then
+  local detectedgame = 'Adopt Me'
   loadstring(game:HttpGet("https://pastebin.com/raw/Z3iAXASr", true))()
 end
 ---------------------------------------------------------------------------------
